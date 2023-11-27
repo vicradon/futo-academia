cd7d588acaaff8d9084b39ac6579a5619154b4c9
Chukwuma Kingsley Chidubem <47928462+ChukwumaKingsley@users.noreply.github.com>
2023-11-27 20:29:27 +0100

    Added search feature to enroll requests

--- src/pages/EnrollmentRequests.tsx
+++ src/pages/EnrollmentRequests.tsx
@@ -1,16 +1,38 @@
 import { useQuery } from "@tanstack/react-query";
 import http from "../utils/http";
-import { Center, Flex, Grid, Input, InputGroup, InputLeftAddon, Text } from "@chakra-ui/react";
+import { Center, Flex, Grid, Input, InputGroup, InputLeftAddon, Select, Text } from "@chakra-ui/react";
 import Loader from "../components/Loaders";
 import { SearchIcon } from "@chakra-ui/icons";
+import CourseStudents from "./CourseStudents";
+import { useEffect, useState } from "react";
+
+interface CourseStudents {
+  search: string,
+  level: any,
+}
 
 function EnrollmentRequests({id}: any) {
 
-  const { data, isLoading } = useQuery({
+  const [filterParams, setFilterParams] = useState<CourseStudents>({search: "", level: ""})
+
+  const { data, isLoading, refetch } = useQuery({
 		queryKey: ["getStudents", id],
-		queryFn: () => http.get(`/students/enrolled/${id}/requests`).then((r) => r.data),
+		queryFn: () => http.get(`/students/enrolled/${id}/requests`, {
+      params : {
+        search: filterParams.search,
+        level: filterParams.level
+    }}).then((r) => r.data),
 		onError: (err) => console.log("error", err),
 	});
+
+  const handleChange = (e: any) => {
+		setFilterParams({ ...filterParams, [e?.target?.name]: e?.target?.value });
+	};
+
+  useEffect(() => {
+    refetch()
+  }, [filterParams])
+  
 
   if (isLoading) return <Loader />;
 
@@ -19,9 +41,16 @@ function EnrollmentRequests({id}: any) {
     <Flex flexDir={"column"}>
       <Center my={5}>
         <InputGroup gridArea={"search"} backgroundColor={"white"} rounded={"md"} maxWidth={{ base: "90%", md: "50%" }}>
-          <Input type="text" placeholder="Search..."  name="search"/>
+          <Input type="text" placeholder="Name or Reg. No." name="search" value={filterParams.search} onChange={handleChange}/>
           <InputLeftAddon backgroundColor={"white"} children={<SearchIcon />} />
         </InputGroup>
+        <Select maxW={"120px"} name="level" placeholder="All level" bg={"white"} value={filterParams.level} onChange={handleChange}>
+          <option value={100}>100</option>
+          <option value={200}>200</option>
+          <option value={300}>300</option>
+          <option value={400}>400</option>
+          <option value={500}>500</option>
+        </Select>
       </Center>
       <Grid templateColumns={"0.8fr 2.5fr 1.8fr 1fr 1fr 0.8fr 0.8fr"} border={"none"} fontSize={{base: "xs", md: "md"}}>
         <Text bgColor={"#343680"} textColor={"white"} border={"none"} p={3}>S/N</Text>
